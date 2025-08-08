4d11c94d6265ec60366e56d41b284903d7d5a921
Abhinandan Saini <123210753+AbhinandanS4@users.noreply.github.com>
2025-08-08 18:04:06 +0530

    Update settings.py

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-gnz#&dlcopf&#e^$e=$+c87qd&82-5@=g97qac6mxyc#(9hl0-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app', '.onrender.com']
 
 
 # Application definition
@@ -132,3 +132,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
