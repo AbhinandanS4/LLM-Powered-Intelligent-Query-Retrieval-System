f04d0055434c5382d438c36db1e9918eb0f66a94
Abhinandan Saini <123210753+AbhinandanS4@users.noreply.github.com>
2025-08-08 19:26:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 # exit on error
 set -o errexit
 
-# Install system packages required for compilation
+# Install necessary system packages for compilation
 apt-get update
-apt-get install -y build-essential libssl-dev libffi-dev
+apt-get install -y build-essential libssl-dev libffi-dev pkg-config python3-dev
 
-# Install Python dependencies
+# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Run Django commands
+# Run Django commands to prepare the application
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Finally, start the Gunicorn server
+# Make sure the path to your wsgi file is correct here.
+exec gunicorn my_project.my_project.wsgi:application
